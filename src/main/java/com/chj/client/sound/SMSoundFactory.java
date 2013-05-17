1821517951f2415e0c429d9e7dcdce97cc5cbc06
Ryan Caloras <rcaloras@gmail.com>
2013-05-16 23:26:26 -0400

    New Snapshot, general cleanup

--- src/main/java/com/chj/client/sound/SMSoundFactory.java
+++ src/main/java/com/chj/client/sound/SMSoundFactory.java
@@ -1,7 +1,5 @@
 package com.chj.client.sound;
 
-import java.util.ArrayList;
-
 
 import com.chj.gwt.client.soundmanager2.Callback;
 import com.chj.gwt.client.soundmanager2.SMSound;
@@ -11,45 +9,37 @@ public class SMSoundFactory implements SoundFactory {
 
 	private static final String SOUND_ID = "soundID";
 
-	/*SoungManager and player related variables*/
-	//SoundManger to play our sound
+	// SoundManger to play our sounds
+
 	private SoundManager sm;
 
-
-	/* The queue of requests */
-	private ArrayList<String> jukeboxQueue = new ArrayList<String>();
-
-	Boolean isPaused = false;
-
-
 	public SMSoundFactory(){
-		sm = SoundManager.quickStart();
+		sm = SoundManager.quickStart(null, 9, true);
+
 	}
 
 
-	public void playSong(String song, Callback callback) {                                                                                                                                                                           
+	public void playSong(String song, Callback callback) {
 
-		//Delete the instance of the old song before playing a new one                                                                                                                                                                       
-		if(!sm.getSoundIDs().isEmpty()){                                                                                                                                                                                                     
-			sm.destroySound(SOUND_ID);                                                                                                                                                                                                      
-		}                                                                                                                                                                                                                                    
+		// Delete the instance of the old song before playing a new one
+		if(!sm.getSoundIDs().isEmpty()){
+			sm.destroySound(SOUND_ID);
+		}
 
-		/* Need to setup our options to callback */                                                                                                                                                                                          
-		if(callback != null){                                                                                                                                                                                                                
-			sm.getDefaultOptions().onFinish(callback);                                                                                                                                  
-		}                                                                                                                                                                                                                                    
-		System.out.println("playing song "+song);                                                                                                                                                                                                                                                                                                                                             
-		sm.createSound(SOUND_ID, song);                                                                                                                                                                                                                                                                                                             
+		/* Need to setup our options to callback */
+		if(callback != null){
+			sm.getDefaultOptions().onFinish(callback);
+		}
+		System.out.println("playing song "+song);
+		sm.createSound(SOUND_ID, song);
 		sm.play(SOUND_ID);
-		isPaused = false;
 	}
 
 	public void stopPlaying() {
 		// TODO Auto-generated method stub
 		System.out.println("Stopping all music!");
 		sm.stopAll();
-		//Should signal event here
-		//eventbus.fireEvent(new SongFinishedEvent(song))
+		// Maybe fire some event event here on an eventbus
 
 	}
 
@@ -61,12 +51,10 @@ public class SMSoundFactory implements SoundFactory {
 
 	public void pausePlaying() {
 		sm.pause(SOUND_ID);
-		isPaused = true;
 	}
 
 	public void resumePlaying() {
 		sm.resume(SOUND_ID);
-		isPaused = false;
 	}
 
 
@@ -82,12 +70,6 @@ public class SMSoundFactory implements SoundFactory {
 
 		});
 	}
-
-
-	public Boolean getIsPaused() {
-		return isPaused;
-	}
-
 
 	public void onStartup(Callback callback ){
 		sm.onReady(callback);
